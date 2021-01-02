eebbd859eaf2b4204d922fdedd7b74804b59c885
saif <mohamedhawas123@gmail.com>
2021-01-02 13:32:56 +0200

    likes and unlike and jquery

--- images/views.py
+++ images/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .form import ImageCreateForm
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
 
 
 @login_required
@@ -27,5 +30,25 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request,  'images/image/detail.html', {'secatioj': 'images', 'image': image})
+    return render(request,  'images/image/detail.html', {'section': 'images', 'image': image})
     
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == "like":
+                Image.users_like.add(request.user)
+            else:
+                Image.users_like.remove(request.user)
+            return JsonResponse({'status' : 'ok'})
+        
+        except:
+            pass
+    
+    return JsonResponse({'status': 'error'})
+
--- vk/settings.py
+++ vk/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'images'
+    'images',
+    'easy_thumbnails',
     
 ]
 
