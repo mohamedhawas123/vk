148b949f85879b3b7df3f851b6dcb2628541bd91
saif <mohamedhawas123@gmail.com>
2021-01-03 13:56:06 +0200

    ajax implement

--- images/views.py
+++ images/views.py
@@ -5,7 +5,7 @@ from .form import ImageCreateForm
 from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
-
+from images.common.decorators import ajax_required
 
 
 @login_required
@@ -32,19 +32,20 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request,  'images/image/detail.html', {'section': 'images', 'image': image})
     
-
+#@ajax_required
 @login_required
 @require_POST
 def image_like(request):
+    print(request.POST.get("me"))
     image_id = request.POST.get("id")
     action = request.POST.get("action")
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
             if action == "like":
-                Image.users_like.add(request.user)
+                image.users_like.add(request.user)
             else:
-                Image.users_like.remove(request.user)
+                image.users_like.remove(request.user)
             return JsonResponse({'status' : 'ok'})
         
         except:
